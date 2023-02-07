73bd325df4a1fbeb82811e95aac01a5b49c87944
titumiranan123 <titumiranan.gtc@gmail.com>
2023-02-07 15:49:45 +0600

    conseptual session linklist-2 file

--- linklist2.cpp
+++ linklist2.cpp
@@ -1,19 +1,64 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class node{
-    public:
+class node
+{
+public:
     int val;
-    node* next;
-    node(int val){
+    node *next;
+    node(int val)
+    {
         this->val = val;
-        next = NULL; 
+        this->next = NULL;
     }
 };
-int main(){
-    node *a = new node(10);
-    node *b = new node(11);
-    node *c = new node(12);
-    a->next = b;
-    b->next = c;
-    cout << a->val<<' '<<a->next->val<<" "<<a->next->next->val;
+void insertAthead(node *&head, int value)
+{
+    node *newnode = new node(value);
+    if (head == NULL)
+    {
+        head = newnode;
+        return;
+    }
+    newnode->next = head;
+    head = newnode;
+}
+void insertAttail(node *&head, int value)
+{
+    node *newnode = new node(value);
+    if (head == NULL)
+    {
+        head = newnode;
+        return;
+    }
+    node* tmp = head;
+    while (tmp->next != NULL)
+    {
+        tmp = tmp->next;
+    }
+    tmp->next = newnode;
+}
+void Print_linklist(node *head)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+    while (head != NULL)
+    {
+        cout << head->val << " ";
+        head = head->next;
+    }
+}
+int main()
+{
+    node *head = NULL;
+    // insertAthead(head, 23);
+    // insertAthead(head, 24);
+    // insertAthead(head, 34);
+    // insertAthead(head, 84);
+    // Print_linklist(head);
+    insertAttail(head, 4);
+    insertAttail(head, 14);
+    insertAttail(head, 94);
+    Print_linklist(head);
 }
